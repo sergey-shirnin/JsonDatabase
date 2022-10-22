d5256b1fa6c6351f7dd7f59a861afb5b4ad6e7f1
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-10-22 13:41:10 +1100

    Stage 4

--- server/CommandHandler.java
+++ server/CommandHandler.java
@@ -1,52 +1,53 @@
 package server;
 
 
+import com.google.gson.Gson;
+
+import java.util.Map;
+import java.util.List;
+
 import static server.Main.db;
 import static server.Main.validCommands;
+
 import static server.Main.OK;
 import static server.Main.ERROR;
 
 
 public class CommandHandler {
-    private final String command;
-    private Integer cell;
-    private String value;
+
+    final String command;
+
+    private final String key;
+    private final String value;
 
     String status = OK;
 
-    CommandHandler(String request) {
-        String[] requestArr = request.split("\\s+", 3);
-        this.command = requestArr[0];
-        if (requestArr.length > 1) { this.cell = Integer.parseInt(requestArr[1]); }
-        if (requestArr.length > 2) { this.value = requestArr[2]; }
+    CommandHandler(String requestJSON) {
+        Map<String, String> requestMap = new Gson().fromJson(requestJSON, Map.class);
+        this.command = requestMap.get("type");
+        this.key = requestMap.get("key");
+        this.value = requestMap.get("value");
     }
 
 
     private void setStatus() {
-        if (null != cell && Math.max(1, cell) != Math.min(cell, db.getSize())
-                || null != cell && "get".equalsIgnoreCase(command) && db.get.apply(cell).isEmpty()
+        if (List.of("get", "delete").contains(command)
+                && null == db.get.apply(key)
                 || !validCommands.contains(command)) {
             this.status = ERROR;
         }
     }
 
-    public String processDataBase() {
+    public Map<String, String> processDataBase() {
         setStatus();
-        if (status.equals(OK)) {
-            switch (command) {
-                case "get" -> {
-                    return db.get.apply(cell);
-                }
-                case "set" ->  {
-                    db.set(cell, value);
-                    return status;
-                }
-                case "delete" -> {
-                    db.delete.accept(cell);
-                    return status;
-                }
-            }
+        if (!status.equals(OK)) {
+            return Map.of("response", status, "reason", "No such key");
         }
-        return status;
+        switch (command) {
+            case "set" ->  { db.set(key, value); }
+            case "delete" -> { db.delete.accept(key); }
+            case "get" -> { return Map.of("response", status, "value", db.get.apply(key)); }
+        }
+        return Map.of("response", status);
     }
 }
