689847e964849107ed95f3c316449db334a9bd2a
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-11-29 14:58:24 +1100

    Stage 5

--- server/Database.java
+++ server/Database.java
@@ -1,19 +1,57 @@
 package server;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.*;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static server.Main.dbPath;
+import static server.Main.dbPathTest;
 
 public class Database {
-    static Map<String, String> db;
+    private Map<String, String> db;
 
-    Database(){
-        db = new HashMap<>();
+    Database() {
+        try {
+            File dbFile = new File(dbPathTest);
+            if (dbFile.createNewFile()) {
+                System.out.printf("New db file created @%n%s%n!", dbPathTest);
+                db = new HashMap<>();
+            } else {
+                System.out.printf("Using existing db file @%n%s%n", dbPathTest);
+                BufferedReader reader = new BufferedReader(new FileReader(dbPathTest));
+                db = new Gson().fromJson(reader.readLine(), HashMap.class);
+            }
+        } catch (IOException e) {
+            System.out.printf("Db file read/create failed!\nMsg:%s%n", e.getMessage());
+        }
     }
 
-    public void set(String key, String value) { db.put(key, value); }
-    public Consumer<String> delete = (key) -> db.remove(key);
+    public BiConsumer<String, String> set = (key, value) -> { db.put(key, value);
+        write();
+    };
+
+    public Consumer<String> delete = (key) -> { db.remove(key);
+        write();
+    };
+
     public Function<String, String> get = (key) -> db.get(key);
+
+    public Predicate<String> containsKey = (key) -> db.containsKey(key);
+
+    private void write() {
+        try (FileWriter writer = new FileWriter(dbPathTest)) {
+            writer.write(new GsonBuilder().create().toJson(db));
+        } catch (Exception e) {
+            System.out.printf("Write to db failed!\nMsg:%s%n", e.getMessage());
+        }
+    }
 }
