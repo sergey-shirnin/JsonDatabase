81077a3c518fdfb25cf4eca48ae5cf29f1afadd3
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-11-29 15:01:38 +1100

    Stage 6

--- server/Main.java
+++ server/Main.java
@@ -11,15 +11,17 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+
 public class Main {
     private final static int PORT = 1_666;
 
     final static String dbPath = System.getProperty("user.dir") + "/JSON Database/task/src/server/data/db.json";
     final static String dbPathTest = System.getProperty("user.dir") + "/src/server/data/db.json";
+    final static String path = dbPathTest;
 
     final static Database db = new Database();
 
-    final static int awaitTime = 800;
+    final static int serverShutdownAwait = 800;
 
     static ReadWriteLock lock = new ReentrantReadWriteLock();
     static Lock readLock = lock.readLock();
@@ -29,6 +31,8 @@ public class Main {
     static ExecutorService executor = Executors.newFixedThreadPool(threads);
     static List<Runnable> unfinishedTasks;
 
+    final static String EXIT = "exit";
+
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(PORT)) {
             System.out.printf("Server started @ %d threads!%n", threads);
@@ -36,27 +40,29 @@ public class Main {
                 Socket socket = server.accept();
                 System.out.println("New client session started!");
 
-                if (executor.submit(new Session(socket)).get()) {
+                Future<String> task = executor.submit(new Session(socket));
+
+                while (!task.isDone()) {
+                    System.out.println("Fetching your data...");
+                    Thread.sleep(100);
+                }
+
+                if (task.get().equals(EXIT)) {
                     executor.shutdown();
 
-                    if (!executor.awaitTermination(awaitTime, TimeUnit.MILLISECONDS)) {
+                    if (!executor.awaitTermination(serverShutdownAwait, TimeUnit.MILLISECONDS)) {
                         unfinishedTasks = executor.shutdownNow();
                         System.out.println("Some requests not processed before termination:" +
                                 "\n-> " + unfinishedTasks);
                     } else {
-                        System.out.println("All requests processed before termination");
+                        System.out.println("All requests processed before server shut");
                     }
                 }
             }
         } catch (IOException e) {
-            System.out.printf("Server set up failed!\nMsg:%s%n",
-                    e.getMessage());
-        } catch (ExecutionException e) {
-            System.out.printf("Client session failed at execution!\nMsg:%s%n",
-                    e.getMessage());
-        } catch (InterruptedException e) {
-            System.out.printf("Client session interrupted at execution!\nMsg:%s%n",
-                    e.getMessage());
+            System.out.printf("Server set up failed!\nMsg:%s%n", e.getMessage());;
+        } catch (ExecutionException | InterruptedException e) {
+            System.out.printf("Server client handling failed!\nMsg:%s%n", e.getMessage());
         }
     }
 }
