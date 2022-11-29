f7305935c8176b69fa5061456a7dd4c94c731930
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-11-29 15:01:15 +1100

    Stage 6

--- client/Args.java
+++ client/Args.java
@@ -1,22 +1,39 @@
 package client;
 
+import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.Parameter;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+
 
 public class Args {
+
 @Parameter(names = {"--input", "-in"})
 private String fileName;
+
 @Parameter(names = {"--type", "-t"})
 private String type;
-@Parameter(names = {"--key", "-k"})
-private String key;
-@Parameter(names = {"--value", "-v"})
-private String value;
+
+@Parameter(names = {"--key", "-k"}, converter = Converter.class)
+private JsonElement key;
+
+@Parameter(names = {"--value", "-v"}, converter = Converter.class)
+private JsonElement value;
+
 
 public String getFileName() { return fileName; }
 
 public String getType() { return type; }
 
-public String getKey() { return key; }
+public JsonElement getKey() { return key; }
 
-public String getValue() { return value; }
+public JsonElement getValue() { return value; }
+
+}
+
+class Converter implements IStringConverter<JsonElement> {
+    @Override
+    public JsonElement convert(String value) {
+        return new JsonPrimitive(value);
+    }
 }
